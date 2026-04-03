0558dcc152cdde4cd2b921bd398bff27d5f7bf91
seeu <josealejandro2006dosreis@gmail.com>
2026-04-03 16:43:21 -0400

    First result of n-queens: places the queens on the board but continues iterating.

--- C++/backtracking/n-queens.cpp
+++ C++/backtracking/n-queens.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
+const bool DEBUG = true;
 
 /*
     N - REINAS
     Colocar n reinas en un tablero de ajedréz de n*n, de forma que ninguna ataque a la otra en la 
-    misma fila, columna o diagonal. Se imprime la configuración de los tableros validos resultantes.
+    misma fila, columna o diagonal. Se imprime la configuración del primer tablero válido resultante.
+
+    **Esta solución no busca ser la más eficiente en términos de tiempo o memoria.
 */
+
+struct Coordinate {
+    int x = -1;
+    int y = -1;
+};
 
 class Chessboard {
     /*
@@ -13,135 +21,105 @@ class Chessboard {
         'X' -> Casilla amenazada
         '_' -> Casilla vacia
     */
-    char **board;
     int N;
+    char **board;
+    Coordinate *queenPositions;
 
     public:
-    Chessboard(int n) {
-        N = n;
+    Chessboard(int n) : N(n) {
         board = new char*[N];
-
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N; i++) {
             board[i] = new char[N];
-        
-        for (int j = 0; j < N; j++) 
-            for (int k = 0; k < N; k++) 
-                board[j][k] = '_';
+            for (int j = 0; j < N; j++) board[i][j] = '_';
+        }
+
+        queenPositions = new Coordinate[N];
     }
 
-    // --------------------------------------------------------------------------------------------------
-
-    bool isInBounds(int row, int column) {
+    bool isInBounds(int X, int Y) {
         // x, y pertenece [0, N)
-        return (row >= 0 && row < N) && (column >=0 && column < N);
+        return (X >= 0 && X < N) && (Y >= 0 && Y < N);
     }
 
-    // modifica las casillas a partir de una posición dada, siguiendo el patrón de alcance de la reina.
-    bool markThreats(int currentRow, int currentColumn) {
-        bool continue_ = true;
-        int tempR = currentRow;
-        int tempC = currentColumn;
+    // establece las casillas amenazadas 'X' una vez es colocada una reina en el tablero.
+    void markThreats(int currentX, int currentY) {
+       // cada par (deltaX[i], deltaY[i]) representa un vector de movimiento.
+       // orden: horizontal(izq), horizontal(der), vertical(arr), vertical(abj)
+       //        diag. sup.(izq), diag. sup.(der), diag. inf.(izq), diag. inf.(der)
+       int deltaX[] = {0,  0, -1, 1, -1, -1,  1, 1};
+       int deltaY[] = {-1, 1,  0, 0, -1,  1, -1, 1};
 
-        // 1. Horizontal (izq, der)
-        while(continue_) {
-            tempC--;
-            if (!isInBounds(tempR, tempC)) break;
-            board[tempR][tempC] = 'X';
-        }
+       for (int i = 0; i < 8; i++) {
+            int tempX = currentX + deltaX[i];
+            int tempY = currentY + deltaY[i];
 
-        tempC = currentColumn;
-        while(continue_) {
-            tempC++;
-            if (!isInBounds(tempR, tempC)) break;
-            board[tempR][tempC] = 'X';
-        }
-
-        // 2. Vertical (arr, abj)
-        tempC = currentColumn;
-        while(continue_) {
-            tempR++;
-            if (!isInBounds(tempR, tempC)) break;
-            board[tempR][tempC] = 'X';
-        }
-
-        tempR = currentRow;
-        while(continue_) {
-            tempR--;
-            if (!isInBounds(tempR, tempC)) break;
-            board[tempR][tempC] = 'X';
-        }
-
-        // 3. Diagonales
-
-            // A. Diagonales superiores (izq, der)
-            tempR = currentRow;
-            tempC = currentColumn;
-            while(continue_) {
-                tempR--;
-                tempC--;
-                if (!isInBounds(tempR, tempC)) break;
-                board[tempR][tempC] = 'X';
+            while (isInBounds(tempX, tempY)) {
+                board[tempX][tempY] = 'X';
+                tempX += deltaX[i];
+                tempY += deltaY[i];
             }
-
-            tempR = currentRow;
-            tempC = currentColumn;
-            while(continue_) {
-                tempR--;
-                tempC++;
-                if (!isInBounds(tempR, tempC)) break;
-                board[tempR][tempC] = 'X';
-            }
-
-            // B. Diagonales inferiores (izq, der)
-            tempR = currentRow;
-            tempC = currentColumn;
-            while(continue_) {
-                tempR++;
-                tempC--;
-                if (!isInBounds(tempR, tempC)) break;
-                board[tempR][tempC] = 'X';
-            }
-
-            tempR = currentRow;
-            tempC = currentColumn;
-            while(continue_) {
-                tempR++;
-                tempC++;
-                if (!isInBounds(tempR, tempC)) break;
-                board[tempR][tempC] = 'X';
-            }
+       }
     }
 
-    void n_queens_algorithm(int row = 0, int column = 0, int q_placed = 0) {
-        
-        while(N < q_placed) {
-
-            if (board[row][column] == '_') {
-                board[row][column] = 'Q';
-                markThreats(row, column);
-                row++; // pasamos a la siguiente fila inmediatamente;
-            }
-
-            q_placed++;
-            // if (DEBUG) print();
-            n_queens_algorithm(row, column, q_placed);
+    // limpia todas las casillas 'X' y vuelve a calcularlas a partir de la posición de cada reina.
+    void removeThreats() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) 
+                if (board[i][j] == 'X') board[i][j] = '_';
         }
+
+        for (int k = 0; k < N; k++) {
+            int tempX = queenPositions[k].x;
+            int tempY = queenPositions[k].y;
+
+            if (tempX >= 0 && tempY >= 0) markThreats(tempX, tempY);
+        }
+    }
+
+    void printBoard() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void n_queens_algorithm(int X = 0, int q_placed = 0) {
+        cout << "new instance\n";
+        if (q_placed == N) {
+            // imprimir resultado.
+            return;
+        } 
+
+        for (int currentY = 0; currentY < N; currentY++) {
+            if (DEBUG) {
+                cout << "main cycle iteration\n";
+                cout << "current: board[" << X << "][" << currentY << "] = " << board[X][currentY] << endl;
+            }
+      
+            if (board[X][currentY] == '_') {
+                board[X][currentY] = 'Q';
+                queenPositions[q_placed].x = X;
+                queenPositions[q_placed].y = currentY;
+                if (DEBUG) cout << "current queens: " << q_placed << endl;
+
+                markThreats(X, currentY);
+
+                if (DEBUG) printBoard();
+                n_queens_algorithm(X + 1, q_placed + 1);
+
+                // -------------------------------------
+                board[X][currentY] = '_';
+                queenPositions[q_placed].x = -1;
+                queenPositions[q_placed].y = -1;
+                removeThreats();
+            } 
+        }
+
+
     }
 };
-
-/*
-    pseudocódigo:
-
-    si la casilla está libre:
-    colocar reina
-    establecer casillas amenazadas consultando límites del tablero
-
-    si al momento de establecer una casilla amenazada se encuentra una reina -> volver atrás.
-
-    repetir hasta que iterador = n (iterador: cantidad de reinas colocadas).
-*/
-
-
 
 int main() {
     int n;  
@@ -153,7 +131,7 @@ int main() {
         return -1;
     }
 
-    Chessboard  chessboard(n);
+    Chessboard chessboard(n);
     chessboard.n_queens_algorithm();
 
     return 0;
